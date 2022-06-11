3c9a92ff91f8389c32e3197d8e80e30af4b582fc
SiinKindred <Khatran1006@gmail.com>
2022-06-11 16:33:34 +0700

    DC

--- BE/src/middlewares/decentralization.middleware.js
+++
@@ -1,9 +0,0 @@
-module.exports = (permission) => {
-	return (req, res, next) => {
-		const { user } = req;
-
-		if (user.type == permission) return next();
-
-		return res.UnauthorizedException({ message: "ROLE_DENIED" });
-	};
-};
